18e0a65bd47937846a0a55ff3bac99804a2e07cb
Riley DuBord <thelifeofdu@gmail.com>
2024-02-15 09:56:18 -0600

    import takeLatest to get login working again

--- src/redux/sagas/getItems.saga.js
+++ src/redux/sagas/getItems.saga.js
@@ -1,9 +1,24 @@
-function* getItems(){
+import { put, takeLatest } from 'redux-saga/effects';
 
+
+function* getItems(){
+    try {
+        // Get the movies:
+        const itemsResponse = yield axios.get('/api/shelf');
+        // Set the value of the movies reducer:
+        yield put({
+          type: 'SET_ITEMS',
+          payload: itemsResponse.data
+        });
+      } catch (error) {
+        console.error('getItems error:', error);
+      }
 }
 
+
+
 function* getItemsSaga() {
-    yield takeLatest('GET_ITEMS', getItems);
+    yield takeLatest('FETCH_ITEMS', getItems);
   }
 
 export default getItemsSaga;
