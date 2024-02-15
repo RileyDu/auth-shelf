6d2411bcadfe65ca74730fdf8b1e697ba0bf7187
Seid Redi <seidredi7@gmail.com>
2024-02-15 10:13:51 -0600

    added addItem saga to post item to backend

--- src/redux/sagas/addItem.saga.js
+++ src/redux/sagas/addItem.saga.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 
-function* addItem(){
-
+function* addItem(action){
+  try {
+    yield axios.post('/api/shelf', action.payload);
+    yield put({type: 'FETCH_ITEMS'})
+  } catch (error) {
+    console.error(error);
+  }
+  
 }
 
 function* addItemSaga() {
