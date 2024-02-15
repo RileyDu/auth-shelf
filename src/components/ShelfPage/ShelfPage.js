e4a7efcd9a1166408a20e53f53061f0da9114971
Riley DuBord <thelifeofdu@gmail.com>
2024-02-15 10:12:01 -0600

    did the thing

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 function ShelfPage() {
   const items = useSelector((store) => store.items);
@@ -13,24 +13,24 @@ function ShelfPage() {
 
   return (
     <div className="container">
-      <h2>Shelf</h2>
-      <ul>
-      {items.map((item) => (
-              <li>
-                <img src={item.image_url}></img>
-                <p>{item.description}</p>
-                <button> DELETE </button>
-              </li>
-              // maps thru the movies array from the store
-              // list item was used to allow componetitization 
-            ))}
-      </ul>
       <h2>Add Item</h2>
       <form>
-        <input placeholder='Item Name' type='text'/>
-        <input placeholder='Item URL' type='url'/>
+        <input placeholder="Item Name" type="text" />
+        <input placeholder="Item URL" type="url" />
         <button>SUBMIT ITEM</button>
       </form>
+      <h2>Shelf</h2>
+      <ul>
+        {items.map((item) => (
+          <li>
+            <img src={item.image_url}></img>
+            <p>{item.description}</p>
+            <button> DELETE </button>
+          </li>
+          // maps thru the movies array from the store
+          // list item was used to allow componetitization
+        ))}
+      </ul>
     </div>
   );
 }
